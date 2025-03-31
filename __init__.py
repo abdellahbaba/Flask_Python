4752c5a140ffd5a537ea0dc0cf4df95d0274a50b
abdellahbaba <abdellahbaba2004@gmail.com>
2025-03-31 11:13:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ def exercice(valeur):
     etoiles = ''
     for i in range(valeur):
         etoiles += '*'
-    return etoiles #(abdel)
+    return etoiles #(abdel) //COMM
 
 
 if __name__ == "__main__":
