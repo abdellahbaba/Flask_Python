97107681173eb8762f51a4c42859ac33980997b3
abdellahbaba <abdellahbaba2004@gmail.com>
2025-04-07 10:53:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,29 +1,14 @@
-from flask import Flask
-from flask import render_template
-from flask import json
+a = 0
+b = 1
 
-app = Flask(__name__)
 
-@app.route('/<int:valeur>')
-def pyramide(valeur):
-    resultat = '<pre>'
-    
-    for i in range(1, valeur + 1):
-        # Créer la partie avant le sommet
-        gauche = ''.join(str(x) for x in range(1, i + 1))
-        # Créer la partie après le sommet
-        droite = ''.join(str(x) for x in range(i - 1, 0, -1))
-        
-        # Ajouter les espaces nécessaires pour centrer
-        espaces = ' ' * (valeur - i)
-        
-        # Combiner tout cela
-        resultat += espaces + gauche + droite + '\n'
-    
-    resultat += '</pre>'
-    return resultat
+n = int(input("Entrez le nombre de termes de la suite : "))
 
- 
-if __name__ == "__main__":
-    app.run(debug=True) 
+suite = []
 
+for i in range(n):
+    suite.append(a)
+    a, b = b, a + b
+
+
+print("Résultat :", ", ".join(map(str, suite)))
