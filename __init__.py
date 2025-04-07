414100c47083714bde4bda53b212f73749d84ceb
abdellahbaba <abdellahbaba2004@gmail.com>
2025-04-07 09:52:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,5 +16,13 @@ def generer_pyramide(valeur):
 
 
 generer_pyramide(5)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Ceci est affiché à l’écran du visiteur, pas dans les logs."
+
 
 
